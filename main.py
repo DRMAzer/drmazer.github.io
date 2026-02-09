ea4c8824e07633d55296392d04a882047dd72e2d
DRMAzer <7ded1021@gmail.com>
2026-02-08 23:58:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import requests
 import base64
 import random
-
+import os
 # --- البيانات الأساسية --
 API_TOKEN = '8211772439:AAF_0tUPpyZVIAU29kWadGOg0UWSkV3L6ys'
 ADMIN_ID = 8574641551 
@@ -20,7 +20,7 @@ SHIPPING_MSG = (
 )
 
 # --- بيانات GitHub ---
-import os
+
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
  
 REPO_NAME = 'DRMazer/drmazer.github.io' 
