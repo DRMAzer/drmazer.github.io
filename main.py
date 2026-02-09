550d58ddb356a0c702f2179c4407e19da4015683
DRMAzer <7ded1021@gmail.com>
2026-02-08 22:53:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import base64
 import random
 
-# --- البيانات الأساسية ---
+# --- البيانات الأساسية --
 API_TOKEN = '8211772439:AAF_0tUPpyZVIAU29kWadGOg0UWSkV3L6ys'
 ADMIN_ID = 8574641551 
 CHANNEL_ID = '@midosaadoffichall' 
